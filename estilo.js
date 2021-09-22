cc2ca5b081a2c04c251f1edbac8e6297c4cecce8
alexfuenmayor <estherf.m18@gmail.com>
2021-09-22 12:50:02 -0500

    mi primer commit

---
+++ estilo.js
@@ -0,0 +1,139 @@
+const nombreDepartamento = () => {
+  let departamento = prompt(
+    "Ingresa El nombre de tu Departamento"
+  ).toLowerCase();
+
+  switch (departamento) {
+    case "amazonas":
+      alert("La Capital es: Leticia");
+      break;
+
+    case "antioquia":
+      alert("La Capital es: Medellín");
+      break;
+
+    case "arauca":
+      alert("La Capital es: Arauca");
+      break;
+
+    case "atlántico":
+      alert("La Capital es: Barranquilla");
+      break;
+
+    case "bolivar":
+      alert("La Capital es: Cartagena de Indias");
+      break;
+
+    case "boyacá":
+      alert("La Capital es: Tunja");
+      break;
+
+    case "caldas":
+      alert("La Capital es: Manizales");
+      break;
+
+    case "caquetá":
+      alert("La Capital es: Florencia");
+      break;
+
+    case "casanare":
+      alert("La Capital es: Yopal");
+      break;
+
+    case "cauca":
+      alert("La Capital es: Popayán");
+      break;
+
+    case "cesar":
+      alert("La Capital es: Valledupar");
+      break;
+
+    case "chocó":
+      alert("La Capital es: Quibdó");
+      break;
+
+    case "córdoba":
+      alert("La Capital es: Montería");
+      break;
+
+    case "cundinamarca":
+      alert("La Capital es: Bogotá");
+      break;
+
+    case "guainía":
+      alert("La Capital es: inírida");
+      break;
+
+    case "guaviare":
+      alert("La Capital es: San José del Guaviare");
+      break;
+
+    case "huila":
+      alert("La Capital es: Neiva");
+      break;
+
+    case "la guajira":
+      alert("La Capital es: Riohacha");
+      break;
+
+    case "magdalena":
+      alert("La Capital es: Santa Marta");
+      break;
+
+    case "meta":
+      alert("La Capital es: Villavicencio");
+      break;
+
+    case "nariño":
+      alert("La Capital es: San Juan de Pasto");
+      break;
+
+    case "norte de santander":
+      alert("La Capital es: San José de Cúcuta");
+      break;
+
+    case "putumayo":
+      alert("La Capital es: Mocoa");
+      break;
+
+    case "quindío":
+      alert("La Capital es: Armenia");
+      break;
+
+    case "risaralda":
+      alert("La Capital es: Pereira");
+      break;
+
+    case "san andrés y providencia":
+      alert("La Capital es: San Andrés");
+      break;
+
+    case "santander":
+      alert("La Capital es: Bucaramanga");
+      break;
+
+    case "sucre":
+      alert("La Capital es: Sincelejo");
+      break;
+
+    case "tolima":
+      alert("La Capital es: Ibagué");
+      break;
+
+    case "valle del cauca":
+      alert("La Capital es: Cali");
+      break;
+
+    case "vichada":
+      alert("La Capital es: Puerto Carreño");
+      break;
+
+    case "vaupés":
+      alert("La Capital es: Mitú");
+      break;
+
+    default:
+      alert("Favor: ingresa un departamento de Colombia");
+      break;
+  }
+};
